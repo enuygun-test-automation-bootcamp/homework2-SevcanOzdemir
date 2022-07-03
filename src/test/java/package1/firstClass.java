622d305d90ce5d2a02812b233ff51a1a7e20a25a
sevcanozdemir <sevcaanozdmr@gmail.com>
2022-07-03 03:18:57 +0300

    msg

---
+++ src/test/java/package1/firstClass.java
@@ -0,0 +1,16 @@
+package package1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class firstClass {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demoqa.com/webtables");
+        WebElement searchBox= driver.findElement(By.id("searchBox"));
+    }
+}
